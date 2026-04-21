a737e18c6f7b63190299ea583d0b33e1f1e31aac
Naman Tailor <namanisliving@gmail.com>
2026-04-21 14:36:58 +0530

    Create Task model with schema

---
+++ backend/models/Task.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    status : {
+        type: String,
+        enum: ["pending", "completed"],
+        default: "pending"
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Task", taskSchema);
