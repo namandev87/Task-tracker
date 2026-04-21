30c393251743c0a21613c3e8be2cfbad9a303a89
Naman Tailor <namanisliving@gmail.com>
2026-04-21 12:06:51 +0530

    add route to test server response

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Backend is working");
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
