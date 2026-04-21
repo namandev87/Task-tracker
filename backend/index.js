d743fb7cd54135618a9069a1f30ae072ffa12417
Naman Tailor <namanisliving@gmail.com>
2026-04-21 12:15:33 +0530

    Add GET /tasks endpoint with in-memory data

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,26 @@ const app = express();
 
 const PORT = 3000;
 
+const tasks = [
+    {
+        id: 1,
+        title: "Learn Express",
+        status: "pending"
+    },
+    {
+        id: 2,
+        title: "Build Task API",
+        status: "pending"
+    }
+];
+
 app.get("/", (req, res) => {
     res.send("Backend is working");
 });
+
+app.get("/tasks", (req, res) => {
+    res.json(tasks);
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
