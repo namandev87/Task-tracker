c2d76cb36ae1fbfda4d30df068ae9d0cbb1cc19e
Naman Tailor <namanisliving@gmail.com>
2026-04-21 12:32:41 +0530

    add POST endpoin to save task

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,27 @@ const tasks = [
     }
 ];
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Backend is working");
 });
 
 app.get("/tasks", (req, res) => {
     res.json(tasks);
-})
+});
+
+app.post("/tasks", (req, res) => {
+    const newTask = {
+        id: tasks.length+1,
+        title: req.body.title,
+        status: "pending"
+    };
+
+    tasks.push(newTask);
+
+    res.json(newTask);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
