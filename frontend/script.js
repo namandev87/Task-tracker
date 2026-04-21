f0465074087c9299295d24882fa1424483212c1e
Naman Tailor <namanisliving@gmail.com>
2026-04-21 18:34:17 +0530

    Add task creation from UI

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,8 @@
 const API_URL = "http://localhost:3000/tasks";
 
 const taskList = document.getElementById("taskList");
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
 
 async function fetchTasks() {
     const res = await fetch(API_URL);
@@ -23,5 +25,24 @@ function renderTasks(tasks) {
     taskList.appendChild(li);
   });
 }
+
+addBtn.addEventListener("click", async () => {
+    const title = taskInput.value;
+
+    if(!title.trim()) {
+        return alert("Task cannot be empty");
+    }
+
+    await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ title })
+    });
+
+    taskInput.value="";
+    fetchTasks();
+})
 
 fetchTasks();
