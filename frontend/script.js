0394b2fbab7f34531f05cb1cc9526864be5d138e
Naman Tailor <namanisliving@gmail.com>
2026-04-22 20:00:37 +0530

    Polish code and add full functionality

--- frontend/script.js
+++ frontend/script.js
@@ -14,13 +14,49 @@ async function fetchTasks() {
 function renderTasks(tasks) {
   taskList.innerHTML = "";
 
+
   tasks.forEach(task => {
     const li = document.createElement("li");
 
     li.innerHTML = `
       <span>${task.title} (${task.status})</span>
-      <button>Delete</button>
+      <button class="delete-btn" data-id="${task._id}">Delete</button>
+      <button class="status-btn" data-id="${task._id}">${task.status === "pending" ? "Complete" : "Undo"}</button>
     `;
+
+    const deleteBtn = li.querySelector(".delete-btn");
+
+    deleteBtn.addEventListener("click", async () => {
+        const id =deleteBtn.dataset.id;
+
+        const res = await fetch(`${API_URL}/${id}`, {
+            method: "DELETE"
+        });
+        if(!res.ok){
+            console.error("Failed to delete task");
+        }
+        fetchTasks();
+    });
+
+    const statusBtn = li.querySelector(".status-btn");
+
+    statusBtn.addEventListener("click", async () => {
+        const id = statusBtn.dataset.id;
+
+        const res = await fetch(`${API_URL}/${id}`,  {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "status": task.status === "pending" ? "completed" : "pending"
+            })
+        });
+        if(!res.ok){
+            console.log("Failed to update status");
+        }
+        fetchTasks();
+    });
 
     taskList.appendChild(li);
   });
